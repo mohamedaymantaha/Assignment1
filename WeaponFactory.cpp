f5c44425c49c2b63d3dfbc4b86642ed13f5bd307
mohamedaymantaha <maa16f@my.fsu.edu>
2019-02-03 23:34:02 -0500

    Update WeaponFactory.cpp
    Updated WeaponFactory to include three new weapons

--- WeaponFactory.cpp
+++ WeaponFactory.cpp
@@ -1,7 +1,9 @@
 /* 
  * File:   WeaponFactory.cpp
  * Author: Javier <jrescobara@gmail.com>
- *  
+ * 
+ * Added three new weapons to main
+ * Author: Mohamed Aboulela
  */ 
 
 #include <string>
@@ -9,6 +11,9 @@
 #include "WeaponFactory.h"
 #include "CommonSword.h" 
 #include "CommonSpear.h"
+#include "CommonMace.h"
+#include "SimpleHammer.h"
+#include "CrazyRandomSword.h"
 
 WeaponFactory* WeaponFactory::instance = NULL;
 
@@ -26,6 +31,18 @@ Weapon * WeaponFactory::getWeapon(std::string name) {
     
     if (name.compare("spear") == 0) {
         return new CommonSpear();
+    }
+
+    if (name.compare("mace") == 0) {
+        return new CommonMace();
+    }
+    
+    if (name.compare("hammer") == 0) {
+        return new SimpleHammer();
+    }
+
+    if (name.compare("randomsword") == 0) {
+        return new CrazyRandomSword();
     }
 
     throw "Invalid weapon";
